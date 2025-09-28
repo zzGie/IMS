d36fb17667429f6abde3c9c8b567242fcb3cf32a
zzGie <Rogiecalataxx3@gmail.com>
2025-09-28 21:30:15 +0800

    e

--- MyFirstDjangoProject/MyFirstDjangoProject/settings.py
+++ MyFirstDjangoProject/MyFirstDjangoProject/settings.py
@@ -127,3 +127,19 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],    # keep empty for now, Django will auto-detect app-level templates
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
