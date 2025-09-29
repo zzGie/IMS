0b4e3e9313db60e9531d49b9319b69af178c55c3
zzGie <Rogiecalataxx3@gmail.com>
2025-09-29 18:03:15 +0800

    update

--- MyFirstDjangoProject/MyFirstDjangoProject/urls.py
+++ MyFirstDjangoProject/MyFirstDjangoProject/urls.py
@@ -21,11 +21,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
 ]
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('user.urls')),   # now include works
-]
--- MyFirstDjangoProject/user/admin.py
+++ MyFirstDjangoProject/user/admin.py
@@ -1,12 +1,29 @@
 from django.contrib import admin
 from .models import UserProfile
 
+# Customize admin site headers
 admin.site.site_header = "KINGKHARL"
 admin.site.site_title = "Administrator Area"
 admin.site.index_title = "Welcome to my First CRUD Application"
 
-
+# Register UserProfile with custom admin
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
+    # Columns to show in the list view (table)
     list_display = ('id', 'fullname', 'email', 'gender', 'contact_number', 'username')
+    
+    # Enable search
     search_fields = ('fullname', 'email', 'username')
+
+    # Optional: Add filters on the sidebar
+    list_filter = ('gender',)
+
+    # Optional: Make the edit form more structured
+    fieldsets = (
+        ('Personal Info', {
+            'fields': ('fullname', 'email', 'gender', 'contact_number')
+        }),
+        ('Account Info', {
+            'fields': ('username', 'password')
+        }),
+    )
