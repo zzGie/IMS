6a65b32fbff599e06d32acbe6c3e36cbf1b3f299
zzGie <Rogiecalataxx3@gmail.com>
2025-10-01 17:13:44 +0800

    update

--- MyFirstDjangoProject/MyFirstDjangoProject/urls.py
+++ MyFirstDjangoProject/MyFirstDjangoProject/urls.py
@@ -23,3 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
 ]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)   
--- MyFirstDjangoProject/user/views.py
+++ MyFirstDjangoProject/user/views.py
@@ -1,24 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render, redirect, get_object_or_404
-from .models import UserProfile
-from .forms import UserProfileForm
-
-def user_list(request):
-    users = UserProfile.objects.all()
-    return render(request, 'user/user_list.html', {'users': users})
-
-def user_detail(request, pk):
-    user = get_object_or_404(UserProfile, pk=pk)
-    return render(request, 'user/user_detail.html', {'user': user})
-
-def user_create(request):
-    if request.method == "POST":
-        form = UserProfileForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('user_list')
-    else:
-        form = UserProfileForm()
-    return render(request, 'user/user_form.html', {'form': form})
