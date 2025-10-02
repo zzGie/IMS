8fa1be4e325ef12e9b7e1a1ffad080fa92c91d85
zzGie <Rogiecalataxx3@gmail.com>
2025-10-02 18:57:36 +0800

    update

--- MyFirstDjangoProject/user/urls.py
+++ MyFirstDjangoProject/user/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.index, name='index')
-    
+    path('', views.index, name='index'),
+    path('userlist/', views.userlist, name='userlist'),
+    path('users/add/', views.adduser, name="adduser"),
 ]
--- MyFirstDjangoProject/user/views.py
+++ MyFirstDjangoProject/user/views.py
@@ -1,4 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import UserProfile
+from django.db import IntegrityError
+from django.contrib import messages
+# Create your views here.
 
 def index(request):
-    return render(request, 'user/index.html')
+    user_list = UserProfile.objects.all()
+    context = {
+        'user_list': user_list
+    }
+    return render(request, 'user/index.html', context)
+
+
+def userlist(request):
+    user_list = UserProfile.objects.all()
+    context = {
+        'user_list': user_list
+    }
+    return render(request, 'user/userlist.html', context)
+
+def adduser(request):
+    if request.method == "POST":
+        contact_number = request.POST.get("contact_number")
+        
+        # Check duplicate
+        if UserProfile.objects.filter(contact_number=contact_number).exists():
+            messages.error(request, "Contact number already exists. Please use another.")
+            return redirect("add_user")  # or return render(request, "add_user.html")
+        
+        try:
+            user = UserProfile(
+                fullname=request.POST.get("fullname"),
+                username=request.POST.get("username"),
+                email=request.POST.get("email"),
+                password=request.POST.get("password"),
+                contact_number=contact_number,
+                gender=request.POST.get("gender"),
+                address=request.POST.get("address"),
+                user_image=request.FILES.get("user_image"),
+            )
+            user.save()
+            messages.success(request, "User added successfully!")
+            return redirect("userlist")
+        except IntegrityError:
+            messages.error(request, "Error saving user. Try again.")
+            return redirect("adduser")
+    
+    return render(request, "user/adduser.html")
